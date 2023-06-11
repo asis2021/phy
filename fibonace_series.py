bff902283910e18f77bb0e3aca1272544e1f7a89
asis2021 <129278532+asis2021@users.noreply.github.com>
2023-06-11 11:08:57 +0530

    Add files via upload

---
+++ fibonace_series.py
@@ -0,0 +1,18 @@
+def febo(n):
+    first_num=0
+    sec_num=1
+    r=n-3
+
+    if n == 1:
+        print(first_num)
+    elif n == 2:
+        print(first_num,sec_num)
+    else:
+         print(first_num, sec_num,end=' ')
+         for i in range(n-2):
+             third_num= first_num + sec_num
+             first_num=sec_num
+             sec_num=third_num
+             print(third_num,end=' ')
+nu=int(input("Enter a number:"))
+febo(nu)
